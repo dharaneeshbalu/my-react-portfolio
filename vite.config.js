95fad0ea2d1dd7ba773034eae6adf444f0bb7657
dharaneeshbalu <dharaneeshbalu2006@gmail.com>
2026-01-28 23:16:02 +0530

    Fix: Add base path to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vite.js.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 1. Force the app to load from the root directory
+  base: '/', 
+  
+  // 2. Optional: Ensure the build folder is always named 'dist'
+  build: {
+    outDir: 'dist',
+  }
 })
